22747413bb53a66d5d1606eb0b749720b2f0cca5
John <john@Johns-Mac-mini.local>
2021-07-07 18:31:26 +0530

    Updated code

--- app/src/main/java/com/example/bitbucketlearning/MainActivity2.kt
+++ app/src/main/java/com/example/bitbucketlearning/MainActivity2.kt
@@ -2,12 +2,10 @@ package com.example.bitbucketlearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Toast.makeText(applicationContext,"SDF SDF SDF SDF ",Toast.LENGTH_LONG).show()
+        setContentView(R.layout.activity_main2)
     }
 }
